51ba9f9e3b2654bb194397d5b0d7f7185e8d0749
Angad-Raut <85058287+Angad-Raut@users.noreply.github.com>
2024-09-08 16:31:21 +0530

    changes commited

--- src/main/java/com/projectx/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/com/projectx/authentication_service/AuthenticationServiceApplication.java
@@ -2,15 +2,21 @@ package com.projectx.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthenticationServiceApplication {
+public class AuthenticationServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AuthenticationServiceApplication.class);
+	}
 }
